0a043e0f550d17190f657cdd31b701dfdb37352f
CAFE2l <pciskolargx@gmail.com>
2026-03-17 17:28:12 -0300

    starting nextjs

---
+++ treinablog/app/settings/card.tsx
@@ -0,0 +1,8 @@
+export default function Card(){
+  return(
+    <div>
+      <h1>Card 1</h1>
+      <p>Descrição </p>
+    </div>
+  )
+}
---
+++ treinablog/app/settings/page.tsx
@@ -0,0 +1,12 @@
+import Card from "./card.tsx";
+
+export default function Settings(){
+  return (
+    <>
+    <h1>Profile Settings</h1>
+    <Card />
+    </>
+  )
+}
+
+
